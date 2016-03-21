daf200a8093e9e6ddc9ebb22659573e4bbfed33d
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2016-03-21 08:04:06 -0700

    Added methods to support loading CrazyNateSharpDisasm.dll from unmanaged code with ICorRuntimeHost

---
+++ SharpDisasm/DependencyFetcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyNateSharpDisasm
+{
+  public static class DependencyFetcher
+  {
+    public static string GetPathToThisAssembly()
+    {
+      // Get the full path of this dll
+      string thisDllUri = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+      string thisDllPath = (new Uri(thisDllUri)).LocalPath;
+      return thisDllPath;
+    }
+  }
+}
---
+++ SharpDisasm/ManagedEntryPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrazyNateSharpDisasm
+{
+  public static class ManagedEntryPoint
+  {
+    public static string EntryTypeName { get { return "CrazyNateSharpDisasm.ManagedEntryPoint"; } }
+    public static string EntryMethodName { get { return "Enter"; } }
+    public static string EntryArgument { get { return "hello"; } }
+
+    public static int Enter(string arg)
+    {
+      // This method doesn't really need to do anything on load
+      return 1;
+    }
+  }
+}
